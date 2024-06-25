05525d1d2c65673f48a5919e44185f7cfe6cd86e
Jorge Estrada <jorgeaestrada45@gmail.com>
2024-06-24 23:57:11 -0700

    Initial commit

---
+++ app/src/main/java/estrada/jorge/myfeelings_estrada/MainActivity.kt
@@ -0,0 +1,11 @@
+package estrada.jorge.myfeelings_estrada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
